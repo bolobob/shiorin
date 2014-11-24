b7c10718320b8a3d3c2aff957e42823b8347e6ef
bolobob <aokishinichi@gmail.com>
2014-11-24 21:46:38 +0900

    modify: 最新記事へのリンクを設定

--- js/app.js
+++ js/app.js
@@ -13,3 +13,19 @@ $(function() {
 
   app.init();
 });
+
+google.load("feeds", "1");
+function initialize() {
+  var feed = new google.feeds.Feed("http://feedblog.ameba.jp/rss/ameblo/tamai-sd");
+  var $link = $('.latest');
+  var $title = $('.latest .title');
+  feed.setNumEntries(1);
+  feed.load(function(result) {
+    if (!result.error) {
+      var entry = result.feed.entries.pop();
+      $title.text(entry.title)
+      $link.attr('href', entry.link)
+    }
+  });
+}
+google.setOnLoadCallback(initialize);
