8ccd0f1befd807c2a42c6efde88d8077fc11a0f0
bolobob <aokishinichi@gmail.com>
2014-11-24 22:51:38 +0900

    modify: 経過時間を表示

--- js/app.js
+++ js/app.js
@@ -14,17 +14,42 @@ $(function() {
   app.init();
 });
 
+moment.locale('ja');
 google.load("feeds", "1");
+function getElapsedTime(date) {
+  return moment().diff(moment(date), 'years') ||
+      moment().diff(moment(date), 'days') ||
+      moment().diff(moment(date), 'hours') ||
+      moment().diff(moment(date), 'minutes');
+}
+function getTimeUnit(date) {
+  var timeUnit = '';
+  if (moment().diff(moment(date), 'years')) {
+    timeUnit = '年';
+  } else if (moment().diff(moment(date), 'days')) {
+    timeUnit = '日';
+  } else if (moment().diff(moment(date), 'hours')) {
+    timeUnit = '時間';
+  } else if (moment().diff(moment(date), 'minutes')) {
+    timeUnit = '秒';
+  }
+
+  return timeUnit;
+}
 function initialize() {
   var feed = new google.feeds.Feed("http://feedblog.ameba.jp/rss/ameblo/tamai-sd");
   var $link = $('.latest');
   var $title = $('.latest .title');
+  var $elapsedTime = $('.elapsed-time');
   feed.setNumEntries(1);
   feed.load(function(result) {
+    var elapsedTime = '';
     if (!result.error) {
       var entry = result.feed.entries.pop();
-      $title.text(entry.title)
-      $link.attr('href', entry.link)
+      $title.text(entry.title);
+      $link.attr('href', entry.link);
+      elapsedTime = getElapsedTime(entry.publishedDate) + getTimeUnit(entry.publishedDate);
+      $elapsedTime.text(elapsedTime);
     }
   });
 }
